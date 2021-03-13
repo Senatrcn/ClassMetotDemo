790f1b9552285913de4e80b6b05a204fba6ab416
sena tercan <sena_tercan@hotmail.com>
2021-03-13 14:36:40 +0300

    3. Gün Ödev 3

---
+++ ClassMetotDemo/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string NationalId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+
+    }
+}
---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri eklendi : " + customer.FirstName + customer.LastName);
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Müşteri silindi : " + customer.FirstName + customer.LastName);
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Müşteri bilgileri güncellendi : " + customer.FirstName + customer.LastName);
+        }
+        public void GetAll(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName + customer.LastName);
+            }
+        }
+    }
+}
